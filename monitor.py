ff78a019293d1e614e6a7904f23fc40a27787c85
Alican <alicandonmez90@gmail.com>
2018-02-24 17:16:52 +0100

    transfered web app to console program

--- monitor.py
+++ monitor.py
@@ -7,13 +7,28 @@ and also satisfying requirements or not.
 Author: Alican Donmez - alicandonmez90@gmail.com.
 """
 
+from flask import Flask, render_template
 import logging
+import threading
 import csv
 import sys
 import time
 from helpers import monitor_site, current_datetime
 
 # logging.basicConfig(level=logging.INFO)
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    handle = open('log_last.csv')
+    reader = csv.reader(handle)
+    datetime = reader.__next__()[1]
+    header = reader.__next__()
+    return render_template('index.html', reader=reader, datetime=datetime,
+                           header=header)
 
 
 def main():
